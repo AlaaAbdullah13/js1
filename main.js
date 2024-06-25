acf9b5c29e2e84645f1bb170c84dce59dcf17ebc
AlaaAbdullah13 <alaaabdallah3336@gmail.com>
2024-06-25 12:52:21 +0300

    frst upload

---
+++ main.js
@@ -0,0 +1,56 @@
+// Which variable can't I use to store "x" in this Example?
+/*(let-var-const) write here : const*/   x = 5;
+x = 7;
+
+/* In This Example You should write instead of dots 
+the name of this "data type". */
+
+let   M  = 1         ;    //number
+const i  = "1"       ;    //string
+var   c  = true      ;    //Boolean
+let   r  = null      ;    //null (in object)
+const o  = NaN       ;    //number
+const t  = undefined ;    //undefined
+var   s  = {
+            track:"Frontend"
+           }         ;  // object
+let   of =   ["HTML","CSS","JS",3] ; //array
+//-------------------------------------------------------------------------------
+// Here's some sentences that I want to print in the console in the same shape
+
+console.log("It takes a great deal of bravery to stand up to our enemies, \n"
++"but just as much to stand up to our friends.");
+
+/*
+Result in console : ...
+    It takes a great deal of bravery to stand up to our enemies,
+    but just as much to stand up to our friends.
+*/
+//#############################################################################
+
+//*here two ways to get the result , I need you to write these two ways
+console.log("After all this time?\n \"Always\".");   //way number 1
+
+ console.log('After all this time?\n "Always".');   //way number 2
+
+
+
+/*
+Result in console : ...
+    After all this time?
+    "Always".
+*/
+
+//#############################################################################
+
+//*here two ways to get the result , I need you to write these two ways
+console.log('Fear of a \'name\' only increases fear of the thing itself.'); //way number 1
+
+console.log("Fear of a 'name' only increases fear of the thing itself.");  //way number 2
+
+/*
+Result in console : ...
+    Fear of a 'name' only increases fear of the thing itself.
+*/
+
+
